994d5f58376a01bd928a5e904eefa1a8ddceb559
EC2 Default User <ec2-user@ip-172-31-34-168.ec2.internal>
2023-04-19 22:35:09 +0000

    atividade

---
+++ at.05.py
@@ -0,0 +1,9 @@
+#5Escreva uma função que receba um dicionário como entrada e retorna uma nova lista contendo apenas as chaves
+
+dicionario = {'thallyssonn':'estudante','anderson':'professor', 'pc':'computador'}
+
+def ordenar_chaves(dicionario):
+    chaves = dicionario.keys()
+    print(chaves)
+        
+ordenar_chaves(dicionario)
